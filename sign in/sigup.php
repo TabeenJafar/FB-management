2c49df628b2c9227f4c129f5f48843464784617d
TabeenJafar <tabeenjafar@gmail.com>
2025-04-10 21:51:52 +0100

    code

--- sign in/sigup.php
+++ sign in/sigup.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-// Dummy example - This should be replaced with actual database storage
+
 $users = [
     'user123' => 'password123',
 ];
@@ -11,11 +11,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Check if username exists (dummy check)
+    /
     if (isset($users[$username])) {
         echo "Username already exists. Please choose another one.";
     } else {
-        // Save user data (in a real application, save this to a database)
+        
         $users[$username] = $password;
 
         
